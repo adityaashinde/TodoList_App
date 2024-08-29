96389cc5f2f5d090ca82cfde31b16f0c3f6a7d70
adityaashinde <adityashinde4580@gmail.com>
2024-08-29 09:04:26 +0530

    google_fonts package added and also asset(images) added

---
+++ lib/welcome_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WelCome extends StatelessWidget {
+  const WelCome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+    ));
+  }
+}
